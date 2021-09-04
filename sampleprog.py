51377d184afd7fd2995ec1e1e79de1e8acf2b815
alain <cuban.quant@gmail.com>
2021-09-03 21:12:52 -0400

    added a Car class

--- sampleprog.py
+++ sampleprog.py
@@ -1,3 +1,18 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def display(self):
+        output_maker = "I'm a " + self.make
+        output_model = f"My model is {self.model}"
+        print(output_maker)
+        print(output_model)
+
 
 if __name__ == "__main__":
-    print("Something")
+    car1 = Car("Honda", "Civic")
+    car1.display()
+
+    car2 = Car("Toyota", "Prius")
+    car2.display()
